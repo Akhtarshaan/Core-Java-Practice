9c06b29c472acf40909868fa27eb1fde5fe4c89c
Shayan Akhtar <111417437+Shayanakhtar37@users.noreply.github.com>
2022-10-14 11:47:59 +0530

    Add files via upload

---
+++ 14th OCT/DataTypeDemo.java
@@ -0,0 +1,22 @@
+/*
+Program: To understand various data types in java
+@author: Shayan Akhtar
+@Date: 14th OCT 2022
+*/
+
+//declaring a class
+class DataTypeDemo
+{
+	//main method
+	public static void main(String args[])
+	{
+	// declaring local variables
+	int number1=78;
+	int number2=59;
+	// calculating the sum
+	int sum = number1+ number2;
+	System.out.println("The Sum is :"+sum); //printing the value of sum
+}
+// end of main
+}
+// end of class DataTyprDemo
---
+++ 14th OCT/DataTypeDemo2.java
@@ -0,0 +1,26 @@
+/*
+Program: To understand various data types in java
+@author: Shayan Akhtar
+@Date: 14th OCT 2022
+*/
+class DataTypeDemo2
+{
+	public static void main(String args[])
+	{
+		//declaring and initializing local variable
+		String name="Shayan Akhtar";
+		char gender='M';
+		int age=21;
+		float marks=85.4f;
+		boolean pass=true;
+		
+		//printing the value of all variables
+		System.out.println("Name:"+ name);
+		System.out.println("Gender:"+ gender);
+		System.out.println("Age:"+ age);
+		System.out.println("Marks:"+ marks);
+		System.out.println("Pass:"+ pass);
+	}
+	//end of main
+}
+//end of class DataTypedemo2
