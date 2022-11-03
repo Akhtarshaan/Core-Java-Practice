141efe1d2248157f9061e02eb1b364282409052a
r <r@DESKTOP-OB7E174>
2022-11-03 08:05:18 +0530

    25th OCT 2022

---
+++ LoopsDemoo/src/com/demoloop/Test.java
@@ -0,0 +1,28 @@
+ /*
+Program: Demo class to test package.
+Author: Shayan Akhtar
+Date: 25th Oct 2022  
+*/
+
+//declaring a package
+package com.demoloop;
+
+//declaring a class to test package demo
+public class Test {
+
+	//main method
+	public static void main(String[] args) {
+		
+	//creating a object of Demo class	
+	Demo object = new Demo();
+	
+	//calling the method
+	object.printValue();
+	
+	//calling static directly with name of class: no object required
+	Demo.printMessage();
+	
+	}
+	//end of main method
+}
+//end of class.
---
+++ LoopsDemoo/src/com/demoloop2/Test2.java
@@ -0,0 +1,31 @@
+ /*
+Program: Demo class to test package.
+Author: Shayan Akhtar
+Date: 25th Oct 2022  
+*/
+
+//declaring a package
+package com.demoloop2;
+
+//importing a package
+import com.demoloop.*;
+
+public class Test2 {
+	
+	    //main method
+		public static void main(String[] args) {
+			
+		//creating a object of Demo class	
+		Demo object = new Demo();
+		
+		//calling the method
+		//all method should be public 
+		object.printValue();
+		
+		//calling static directly with name of class - No object required
+		Demo.printMessage();
+
+		}
+		//end of main method
+}
+//end of class.
