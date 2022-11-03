47f2109c618b7c7f52def41c5147a07f50b0a717
r <r@DESKTOP-OB7E174>
2022-11-03 07:41:03 +0530

    25th OCT 2022

---
+++ LoopsDemo/src/com/demoloop/Demo.java
@@ -0,0 +1,31 @@
+ /*
+Program: Demo class to test package.
+Author: Shayan Akhtar
+Date: 25th Oct 2022  
+*/
+
+//creating a package
+package com.demoloop;
+
+//declaring class
+public class Demo {
+	
+	    //declaring a instance variable
+		int myValue = 25;
+		
+		//method to print value
+		public void printValue()
+		{
+			System.out.println("Value is :"+myValue);
+			
+		}
+		
+		//static method to print message
+		public static  void printMessage()
+		{
+		System.out.println("Welcome in Java");
+		}
+		//end of method
+}
+//end of class
+
